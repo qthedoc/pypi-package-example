cb6d62311038663669bdb1d778bfe7d187467b45
Quinn Marsh <wquinnmarsh@gmail.com>
2026-02-09 03:20:55 -0800

    fix: package name

---
+++ src/pypi_package_example_qthedoc/__init__.py
@@ -0,0 +1,4 @@
+from pypi_package_example_qthedoc.main import answer_to_life
+
+__version__ = "0.1.0"
+__all__ = ["answer_to_life"]
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,4 @@
-from pypi_package_example import answer_to_life
+from pypi_package_example_qthedoc import answer_to_life
 
 def test_answer_to_life():
 
